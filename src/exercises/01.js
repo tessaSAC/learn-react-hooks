4aeb2fe30e0490fc4a81ab549bd30c941460c3b0
tessaSAC <tessaSAC@users.noreply.github.com>
2019-07-23 14:32:29 -0400

    updates notes

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -10,8 +10,11 @@ function Counter() {
   // 🐨 render the count you get from React.useState inside the button and use
   //    your increment function as the onClick handler.
 
-  const [ count, setCount ] = useState(0)  // `useState` returns a function that updates `count`; takes initial value as arg
-  // called `useState` bc state is only initialized on the first call
+  const [ count, setCount ] = useState(0)
+  // `useState` returns [ state's current value, a function that updates `count`]
+  // Takes initial value as arg
+  // Called `useState` bc state is only initialized on the first call
+  // Looks like to have multiple items they will all be distinct calls to `useState`?
 
   const increment = _ => setCount(count + 1)  // Why do I need to use `setCount`? Is it b/c `count` is primitive?
 
