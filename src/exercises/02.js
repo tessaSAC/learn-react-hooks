bf8f21d3151f3cd385a182b57e93cf332f3dc51d
tessaSAC <tessaSAC@users.noreply.github.com>
2019-07-23 15:04:14 -0400

    finishes exercise 02

--- src/exercises/02.js
+++ src/exercises/02.js
@@ -4,11 +4,25 @@ import React from 'react'
 // 🐨 Make a custom hook called useCounter that accepts the step and
 // initialCount and returns the count and increment functions
 
+function useCounter(initialCount = 0, step = 1) {
+  const [ count, setCount ] = React.useState(initialCount)
+  const increment = _ => setCount(count + step)
+
+  return [ count, increment]
+}
+
+
+
 function Counter({step = 1, initialCount = 0}) {
   // 💣 remove this (or move it to your custom hook)
-  const [count, setCount] = React.useState(initialCount)
-  const increment = () => setCount(c => c + step)
+  // const [count, setCount] = React.useState(initialCount)
+  // const increment = () => setCount(c => c + step)
+
+
+
   // 🐨 Use your custom useCounter hook to get `count` and `increment`
+  const [ count, increment ] = useCounter(initialCount, step)
+
   return <button onClick={increment}>{count}</button>
 }
 
